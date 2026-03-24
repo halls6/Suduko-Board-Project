9204eadf73e616b68bc39caa49a7577d088f607c
Sofia <halls6@tcnj.edu>
2026-03-24 13:21:25 -0400

    created functions to check rows and columns for 27 threads

--- main.c
+++ main.c
@@ -23,16 +23,14 @@ int board[9][9]; /* store sudoku numbers */
 /* METHOD 1 (11 THREADS) FUNCS */
 
 /* checks each row if it contains all digits 1-9 */
-void *checkRows(void *param) {
+void *checkRows1(void *param) {
     parameters *data = (parameters*) param;
     int idx = data->index;
     
     int seen[10];
     
     for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 10; j++) {
-            seen[j] = 0;
-        }
+        for (int j = 0; j < 10; j++) { seen[j] = 0; }
 
         for (int j = 0; j < 9; j++) {
             int val = board[i][j];
@@ -48,16 +46,14 @@ void *checkRows(void *param) {
 }
 
 /* checks each col if it contains all digits 1-9 */
-void *checkCols(void *param) {
+void *checkCols1(void *param) {
     parameters *data = (parameters*) param;
     int idx = data->index;
 
     int seen[10];
     
     for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 10; j++) {
-            seen[j] = 0;
-        }
+        for (int j = 0; j < 10; j++) { seen[j] = 0; }
 
         for (int j = 0; j < 9; j++) {
             int val = board[j][i];
@@ -77,28 +73,71 @@ void *checkSquare(void *param) { /* used by method 1 and 2 */
     parameters *data = (parameters *) param;
     int row = data->row;
     int col = data->column;
-    int index = (row / 3) * 3 + (col / 3) + 2;
+    int idx = data->index;
 
     int seen[10];
-    for (int i = 0; i < 10; i++) {
-        seen[i] = 0;
-    }
+
+    for (int i = 0; i < 10; i++) { seen[i] = 0; }
 
     for (int i = row; i < row + 3; i++) {
         for (int j = col; j < col + 3; j++) {
             int val = board[i][j];
-            if (seen[val] != 0) {
-                results[index] = 0; /* returns 0 if duplicates */
+            if (val < 1 || val > 9 || seen[val] != 0) {
+                results[idx] = 0; /* returns 0 if duplicates */
                 pthread_exit(NULL);
             }
             seen[val] = 1;
         }
     }
-    results[index] = 1; /* returns 1 if 1-9 is in square */
+    results[idx] = 1; /* returns 1 if 1-9 is in square */
     pthread_exit(NULL);
 }
 
 /* METHOD 2 (27 THREADS) FUNCS */
+
+/* checks one row if it contains all digits 1-9 */
+void *checkRows2(void *param) {
+    parameters *data = (parameters*) param;
+    int row = data->row;
+    int idx = data->index;
+    
+    int seen[10];
+    
+    for (int j = 0; j < 10; j++) { seen[j] = 0; }
+
+        for (int j = 0; j < 9; j++) {
+            int val = board[row][j]; /* checking only 1 row */
+            if (val < 1 || val > 9 || seen[val] != 0) {
+                results[idx] = 0; /* returns 0 if duplicates */
+                pthread_exit(NULL);
+            }
+            seen[val] = 1;
+        }
+    results[idx] = 1; /* returns 1 if all digits 1-9 are seen */
+    pthread_exit(NULL);
+}
+
+/* checks one col if it contains all digits 1-9 */
+void *checkCols2(void *param) {
+    parameters *data = (parameters*) param;
+    int col = data->column;
+    int idx = data->index;
+    
+    int seen[10];
+    
+    for (int j = 0; j < 10; j++) { seen[j] = 0; }
+
+        for (int i = 0; i < 9; i++) {
+            int val = board[i][col]; /* checking only 1 row */
+            if (val < 1 || val > 9 || seen[val] != 0) {
+                results[idx] = 0; /* returns 0 if duplicates */
+                pthread_exit(NULL);
+            }
+            seen[val] = 1;
+        }
+    results[idx] = 1; /* returns 1 if all digits 1-9 are seen */
+    pthread_exit(NULL);
+}
 
 /* function to re)ad in board from input.txt */
 void readBoard() {
@@ -133,8 +172,8 @@ int main(int argc, char *argv[]) {
     pthread_t threads[11]; /* creating the 11 threads */
 
     /* threads for rows and cols */
-    pthread_create(&threads[0], NULL, checkRows, NULL);
-    pthread_create(&threads[1], NULL, checkCols, NULL);
+    pthread_create(&threads[0], NULL, checkRows1, NULL);
+    pthread_create(&threads[1], NULL, checkCols1, NULL);
 
     /* 9 threads for 3 by 3 square */
     parameters *data[9];
