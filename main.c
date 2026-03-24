8974774c97cfa033cd10e83ae12c79ac88d6eecd
Sofia <halls6@tcnj.edu>
2026-03-23 21:29:48 -0400

    Layout of main.c

---
+++ main.c
@@ -0,0 +1,26 @@
+/*
+ * Names           : Sofia Hall & Nick Viola
+ * File Name       : main.c
+ * Description     : LATER
+ * 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int board[9][9]; /* store sudoku numbers */
+
+/* function to read in board from input.txt */
+void readBoard() {
+
+}
+
+/* function to print out board */
+void printBoard() {
+
+}
+
+/* main function */
+void main() {
+    return 0;
+}
