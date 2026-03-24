f576a3d033bdc47b9f7ff2f24b24dad8a4ee8b7d
Sofia <halls6@tcnj.edu>
2026-03-23 21:49:47 -0400

    Testing function to read in board from input.txt into main.c

--- main.c
+++ main.c
@@ -12,7 +12,14 @@ int board[9][9]; /* store sudoku numbers */
 
 /* function to read in board from input.txt */
 void readBoard() {
+    FILE *fptr = fopen("input.txt", "r");
 
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            fscanf(fptr, "%d", &board[i][j]);
+        }
+    }
+    fclose(fptr);
 }
 
 /* function to print out board */
@@ -21,6 +28,8 @@ void printBoard() {
 }
 
 /* main function */
-void main() {
+int main() {
+    readBoard();
+    printf("%d\n", board[0][0]);
     return 0;
 }
